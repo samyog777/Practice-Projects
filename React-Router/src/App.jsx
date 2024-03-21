5cdb89db576eea0c1b162b0cfa12dd0f9787e4e6
samyog777 <samyogkoirala18@gmail.com>
2024-03-21 22:47:18 +0545

    Learning Router in React

---
+++ React-Router/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ React-Router/src/main.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./Components/Home.jsx";
+import About from "./Components/About.jsx";
+import Contact from "./Components/Contact.jsx";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/about",
+//         element: <About />,
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Route>
+  )
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
